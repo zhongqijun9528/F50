923d286325baba679eec77d14cc4bf8421cce6ff
yanhao1128 <903222765@qq.com>
2018-09-17 16:39:34 +0800

    页面功能完善删除与查询

--- collectiveProject/src/main.js
+++ collectiveProject/src/main.js
@@ -13,9 +13,6 @@ import serviceManagement from './components/serviceManagement/serviceManagement'
 import storeManagement from './components/storeManagement/storeManagement'
 import userManagement from './components/userManagement/userManagement'
 import orderManagement from './components/orderManagement/orderManagement'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI)
 
 // 服务管理
 import addService from "./components/serviceManagement/addService";
--- finallyobject/lovepetexpress/routes/orders.js
+++ finallyobject/lovepetexpress/routes/orders.js
@@ -9,21 +9,24 @@ client.url('localhost:8080');
 
 // 查询
 router.get('/', async function (req, res, next) {
-    
-    let data = await client.get(`/orders`);
+    let type = req.query.type;
+    let text = req.query.text;
+    let page = parseInt(req.query.page);
+    let rows = parseInt(req.query.rows);
+    let obj = { page, rows };
+    if (type) {
+        obj[type] = text;
+    }
+    let data = await client.get(`/orders`, obj);
     res.send(data)
 });
 // 删除
-router.delete(`/:id`, async function (req, res, next) {
-    let id = req.params.id;
-    await client.delete(`/orders/` + id);
-    res.send('success')
-})
-// 查询
-router.get('/orders', async function (req, res, next) {
-    // 会传入一个用户输入的数据进去
-    let input=req.query.input;
-    let data = await client.get(`/orders`, input);
+router.post(`/deleteData`, async function (req, res, next) {
+    let id = req.body.id;
+    console.log(id);
+    let data2 = await client.delete(`/orders/` + id);
+    let data = await client.get("/orders", { page: 1, rows: 5 });
     res.send(data)
-});
+})
+
 module.exports = router;
